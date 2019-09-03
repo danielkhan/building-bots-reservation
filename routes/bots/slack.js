8b9dfd263aec2adfba01b39f2b28f8cc68631d7f
Daniel Khan <danielkhan@car-booth-23-mac.content.linkedin.biz>
2019-09-03 15:14:17 -0700

    finish 01_07

--- routes/bots/slack.js
+++ routes/bots/slack.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
 
 const router = express.Router();
 
@@ -7,8 +8,19 @@ module.exports = (params) => {
   const { config } = params;
 
   const slackEvents = createEventAdapter(config.slack.signingSecret);
+  const slackWebClient = new WebClient(config.slack.token);
 
   router.use('/events', slackEvents.requestListener());
+
+  async function handleMention(event) {
+    return slackWebClient.chat.postMessage({
+      text: 'Hi there, I\'m Resi. What can I do for you?',
+      channel: event.channel,
+      username: 'Resi',
+    });
+  }
+
+  slackEvents.on('app_mention', handleMention);
 
   return router;
 };
